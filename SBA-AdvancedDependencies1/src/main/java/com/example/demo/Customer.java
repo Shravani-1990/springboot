ce56ffac5028d6d4d11eba8fbf486c6b3aa53ecc
Shravani-1990 <shravanin505@gmail.com>
2025-02-12 15:18:59 +0530

    day10

---
+++ SBA-AdvancedDependencies1/src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // Default constructor
+    public Customer() {}
+
+    // Parameterized constructor
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Method to print customer details
+    public void printCustomer() {
+        System.out.println("Customer ID: " + id + ", Name: " + name);
+    }
+}
